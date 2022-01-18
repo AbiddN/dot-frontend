dc809f1299b07698b351132de20cb17ad6fd6b1b
Abyantahta <abyantahta2002@gmail.com>
2022-01-18 13:21:05 +0700

    menambah sign-in

---
+++ src/pages/SignIn.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './SignIn.css';
+import logo from '../images/logo.png'
+// import { Link } from 'react-router-dom';
+
+function SignIn() {
+    return (
+        <div className="sign-in-full-container">
+            <div className="logo">
+                <img src={logo} alt="" />
+            </div>
+            <h2 className='sign-in-text'>SIGN IN</h2>
+        <div className="sign-in-container">
+            <div className="sign-in-content">
+                <div className="input-wrapper">
+                    <h3 className='input-text'>Email :</h3>
+                    <div className="input-icon">
+                    <div className="icon">
+                        <i class="far fa-envelope"></i>
+                    </div>
+                    <input type="text" className="input-box" placeholder='loveuall@rocketmail.com' />
+                    </div>
+                </div>
+                <div className="input-wrapper">
+                    <h3 className='input-text'>Password :</h3>
+                    <div className="input-icon">
+                    <div className="icon">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <input type="Password" className="input-box" placeholder='**********' />
+                    </div>
+                </div>
+                <button className='btn--'>LOGIN</button>
+                <h6 className='login-txt'>don't have an account? <a to='/' > Sign Up </a></h6>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default SignIn
