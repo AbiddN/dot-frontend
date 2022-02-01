13406469fb546eb72d7510bc01732bfa2b55b3b3
Abyantahta <abyantahta2002@gmail.com>
2022-02-01 10:59:53 +0700

    menambah landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,27 +1,28 @@
 import React from "react";
+import Whitelogo from '../images/whitelogo.png'
+import './Landing.scss';
 
 const Landing = () => {
     return (
-        <div className="landing-container">
-            <div className="landing-wrapper">
-                <div className="landing-header">
-                    <div className="landing-header-logo">
-                        <img src="https://i.ibb.co/0jZxXxq/whitelogo.png" alt="logo" />
-                    </div>
-                    <div className="landing-header-title">
-                        <h1>Dot</h1>
-                    </div>
+        <div className="lp_section">
+        <div className="lp_container">
+            <div className="lp_logo">
+                <img src={Whitelogo} alt="" />
+            </div>
+            
+            <div className="lp_area">
+                <div className="lp_words">
+                    <h2>Dots Connects You With Others Through Simplicity.</h2>
                 </div>
-                <div className="landing-body">
-                    <div className="landing-body-text">
-                        <h2>Dot is a social media platform for connecting people with similar interests.</h2>
-                    </div>
-                    <div className="landing-body-button">
-                        <button>Get Started</button>
-                    </div>
+
+                <div className="lp_button-area">
+                    <button href="#" className="lp_button">LOGIN</button>
+                    <button href="#" className="lp_button">SIGN UP</button>
                 </div>
             </div>
         </div>
+        
+    </div>
     );
 }
 
